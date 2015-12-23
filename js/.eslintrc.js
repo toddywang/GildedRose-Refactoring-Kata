550f43aff9852c698904ee086902d46382b95a5b
Ziyi LIU <i@richard.zone>
2015-12-23 17:05:51 +0800

    ziyi: add npm and test

---
+++ js/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "rules": {
+        "semi": 0,
+        "eqeqeq": 0,
+        //"complexity": 15,
+        "max-depth": [2, 4],
+        "indent": [2, 2],
+        "quotes": 0,
+        "linebreak-style": [
+            2,
+            "unix"
+        ]
+    },
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "extends": "airbnb"
+};
